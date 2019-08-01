5dc79cbb2d0fb28441f45c086d2166ef34bf47ef
Kevin Evans <mkevinevans@gmail.com>
2019-08-01 13:57:10 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,11 @@
 // Code Product Component Here
+import { React } from 'react'
+import { Proptypes } from 'prop-types'
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div>Product</div>
+      )
+  }
+}
